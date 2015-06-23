f8724bcba5ecdc62a4daf99cff4510556445bda5
Brian L <brian.londregan@gmail.com>
2015-06-23 16:55:55 -0400

    adding comments to code

--- source/rabbitsandfoxes.py
+++ source/rabbitsandfoxes.py
@@ -10,45 +10,47 @@ def rabbitGrowth():
     global CURRENTRABBITPOP
     
     newRabbits = 0    
-    for i in range(CURRENTRABBITPOP):
-        if (CURRENTRABBITPOP + newRabbits) < MAXRABBITPOP:
+    for i in range(CURRENTRABBITPOP):  #for every rabbit in the forest
+        if (CURRENTRABBITPOP + newRabbits) < MAXRABBITPOP:  #rule: rabbit population has upper limit
             pRabbitRepro = 1 - (float(CURRENTRABBITPOP + newRabbits) / float(MAXRABBITPOP))
-            if random.random() < pRabbitRepro:
+            if random.random() < pRabbitRepro:   #chances of rabbit reproducing
                 newRabbits += 1
         else:
             break
     
-    CURRENTRABBITPOP = CURRENTRABBITPOP + newRabbits
+    CURRENTRABBITPOP = CURRENTRABBITPOP + newRabbits   #new head count
             
 def foxGrowth():
     global CURRENTRABBITPOP
     global CURRENTFOXPOP
     
     foxChanges = 0    
-    for i in range(CURRENTFOXPOP):        
+    for i in range(CURRENTFOXPOP):    #for every fox in the forest    
             pFoxEats = (float(CURRENTRABBITPOP) / float(MAXRABBITPOP))
-            if random.random() < pFoxEats:  #caught one
-                if CURRENTRABBITPOP > 10:
-                    CURRENTRABBITPOP = CURRENTRABBITPOP - 1
-                    if random.random() < (1.0/3.0):
+            if random.random() < pFoxEats:  #caught a rabbit
+                if CURRENTRABBITPOP > 10:  #rule: must be at least 10 rabbits
+                    CURRENTRABBITPOP = CURRENTRABBITPOP - 1  #kill that rabbit
+                    if random.random() < (1.0/3.0):  #chances of fox reproducing given that it ate
                         foxChanges += 1
                 else:
                     break
-            else: #did not catch one
-                if (CURRENTFOXPOP + foxChanges) > 10:
-                    if (random.random() < 0.9):
-                        foxChanges -= 1
+            else: #did not catch a rabbit
+                if (CURRENTFOXPOP + foxChanges) > 10: #rule: must be at least 10 foxes
+                    if (random.random() < 0.9):  #chances of fox dying of hunger
+                        foxChanges -= 1    #kill that fox
 
-    CURRENTFOXPOP = CURRENTFOXPOP + foxChanges
+    CURRENTFOXPOP = CURRENTFOXPOP + foxChanges  #new head count
             
 def runSimulation(numSteps):
+    #run through x time steps and see what the model does to the populations over time
+    #add current counts to these lists every time step to watch population rise or fall
     rabbit_populations = []
     fox_populations = []
 
     for n in range(numSteps):       
         if n == 0:
             if (CURRENTRABBITPOP < 10) or (CURRENTFOXPOP < 10):
-                break
+                break   #rule: again, must be at least 10 rabbits and 10 foxes
                 
         rabbitGrowth()
         rabbit_populations.append(CURRENTRABBITPOP)
@@ -60,6 +62,10 @@ def runSimulation(numSteps):
         
 
 
+
+
+
+#START TEST THEN PLOT
 
 numTrials = 200
 tup = runSimulation(numTrials)
